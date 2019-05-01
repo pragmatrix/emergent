65c43bf0d70955b6e7b90a55276d0eadc6e56519
Armin Sander <armin@replicator.org>
2019-05-01 16:50:56 +0200

    prepare lib testcases

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,7 +1,17 @@
 //! A library that is part of the tests in its role as a "library under test".
 #[test]
 fn test_output_capture() {
-    for i in 0..1000 {
-        println!("CAPTURE_ME: {}", i);
+    println!("CAPTURE_ME");
+}
+
+mod mod_test {
+    #[test]
+    fn test_in_mod_capture() {
+        println!("CAPTURE_ME_IN_MOD")
+    }
+
+    #[test]
+    fn test_expect_fail() {
+        assert_ne!(1, 1)
     }
 }
