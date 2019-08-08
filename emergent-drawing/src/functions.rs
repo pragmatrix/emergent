3e083394df54fcbdb3137cbd5fd70f774f0cb3ea
Armin Sander <armin@replicator.org>
2019-08-08 09:29:01 +0200

    drawing: export paint under functions

--- emergent-drawing/src/functions.rs
+++ emergent-drawing/src/functions.rs
@@ -1,6 +1,7 @@
 pub use crate::circle::circle;
 pub use crate::geometry::bounds::bounds;
 pub use crate::line::{line, line_h, line_v};
+pub use crate::paint::paint;
 pub use crate::point::point;
 pub use crate::rect::rect;
 pub use crate::text::text;
