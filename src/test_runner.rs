a064e5f34cb05ef5df17d3680d4faf24deaf3942
Armin Sander <armin@replicator.org>
2019-08-09 18:27:34 +0200

    use cargo_metadata to capture and parse cargo output

--- src/test_runner.rs
+++ src/test_runner.rs
@@ -3,7 +3,8 @@ use crate::libtest::TestCaptures;
 use cargo::core::compiler;
 use cargo::ops;
 use cargo::ops::{FilterRule, LibRule};
-use std::io::Cursor;
+use std::env;
+use std::io::{Cursor, Write};
 use std::path::{Path, PathBuf};
 
 #[derive(Clone, PartialEq, Eq, Debug)]
@@ -22,55 +23,94 @@ impl TestRunRequest {
 
     pub fn capture_tests(&self) -> Result<TestCaptures, failure::Error> {
         let manifest_path = self.project_directory.join("Cargo.toml");
-        let config = &cargo::Config::default()?;
-        let normalized_path = cargo::util::paths::normalize_path(&manifest_path);
-        dbg!(&normalized_path);
-        let workspace = &cargo::core::Workspace::new(&normalized_path, config)?;
 
-        // build library only for now.
-        let compile_filter = ops::CompileFilter::Only {
-            all_targets: false,
-            // Prefer to test the library of the package for now only.
-            lib: LibRule::True,
-            bins: FilterRule::Just(vec![]),
-            examples: FilterRule::Just(vec![]),
-            tests: FilterRule::Just(vec![]),
-            benches: FilterRule::Just(vec![]),
+        // TODO: verify if this is correct (taken from cargo::Config::new()).
+        let current_dir = env::current_dir().unwrap();
+        let home_dir = cargo::util::config::homedir(&current_dir).unwrap();
+
+        let (test_result, captured) = {
+            /*
+            this code may be used to capture / supporess the error output (for example Fresh lines)
+
+            let mut output = Vec::new();
+            let mut output_cursor = Cursor::new(output);
+            let boxed_output_cursor: Box<dyn Write> = Box::new(output_cursor);
+            let shell = cargo::core::Shell::from_write(boxed_output_cursor);
+            */
+
+            // stdout can not be captured this way there is an actual println! used
+            // although there are ways to capture println! calls I can remember, we need
+            // also capture test output.
+
+            let shell = cargo::core::Shell::new();
+            let config = cargo::Config::new(shell, current_dir, home_dir);
+
+            let normalized_path = cargo::util::paths::normalize_path(&manifest_path);
+            dbg!(&normalized_path);
+            let workspace = &cargo::core::Workspace::new(&normalized_path, &config)?;
+
+            // build library only for now.
+            let compile_filter = ops::CompileFilter::Only {
+                all_targets: false,
+                // Prefer to test the library of the package for now only.
+                lib: LibRule::True,
+                bins: FilterRule::Just(vec![]),
+                examples: FilterRule::Just(vec![]),
+                tests: FilterRule::Just(vec![]),
+                benches: FilterRule::Just(vec![]),
+            };
+
+            let mut compile_options =
+                ops::CompileOptions::new(&config, compiler::CompileMode::Test)?;
+            compile_options.build_config.message_format = compiler::MessageFormat::Json;
+            compile_options.filter = compile_filter;
+
+            let test_options = &ops::TestOptions {
+                compile_opts: compile_options,
+                no_run: false,
+                no_fail_fast: false,
+            };
+
+            // we need a very specific set of arguments to make precise capturing of the output work.
+            let libtest_args: Vec<&str> = vec![
+                "--test-threads",
+                "1",
+                "--nocapture",
+                "-Z",
+                "unstable-options",
+                "--format",
+                "json",
+            ];
+
+            let capture = Capture::stdout();
+            let test_result = ops::run_tests(workspace, test_options, &libtest_args);
+            (test_result, capture.end())
         };
-
-        let mut compile_options = ops::CompileOptions::new(&config, compiler::CompileMode::Test)?;
-        compile_options.build_config.message_format = compiler::MessageFormat::Json;
-        compile_options.filter = compile_filter;
-
-        let test_options = &ops::TestOptions {
-            compile_opts: compile_options,
-            no_run: false,
-            no_fail_fast: false,
-        };
-
-        // we need a very specific set of arguments to make precise capturing of the output work.
-        let libtest_args: Vec<&str> = vec![
-            "--test-threads",
-            "1",
-            "--nocapture",
-            "-Z",
-            "unstable-options",
-            "--format",
-            "json",
-        ];
-
-        let capture = Capture::stdout();
-        let test_result = ops::run_tests(workspace, test_options, &libtest_args);
-        let captured = capture.end();
 
         println!(">>> TEST RESULT: {:?}", test_result);
         println!(">>> CAPTURED BEGIN");
         println!("{}", String::from_utf8_lossy(&captured));
         println!(">>> CAPTURED END");
 
-        let cursor = Cursor::new(captured);
+        let cursor = Cursor::new(&captured);
 
-        TestCaptures::from_output(cursor)
+        // parse messages from cargo:
+        let mut iterator = cargo_metadata::parse_messages(cursor);
+        for msg in &mut iterator {
+            match msg {
+                Ok(msg) => println!("CARGO: {:?}", msg),
+                Err(_) => break,
+            }
+        }
+
+        // and interpret the rest as test captures.
+        let byte_offset = iterator.byte_offset();
+        let rest = &captured[byte_offset..];
+
+        // TODO: perhaps it's better to separate the compilation of the test code and the running of it?
+
+        // TODO: if the test_result failed (the build), we should probably not continue here.
+        TestCaptures::from_output(Cursor::new(rest))
     }
 }
 
