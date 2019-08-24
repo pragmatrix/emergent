dd664ad1c9f8edbb3de21867d4efcb50a41d73db
Armin Sander <armin@replicator.org>
2019-08-24 15:51:01 +0200

    export new function 'block' for creating text blocks

--- emergent-drawing/src/functions.rs
+++ emergent-drawing/src/functions.rs
@@ -4,5 +4,6 @@ pub use crate::line::{line, line_h, line_v};
 pub use crate::paint::paint;
 pub use crate::point::point;
 pub use crate::rect::rect;
+pub use crate::text::block as text_block;
 pub use crate::text::text;
 pub use crate::vector::vector;
