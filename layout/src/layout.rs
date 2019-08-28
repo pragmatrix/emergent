a6e57cbc55c476c55bb7ccb3c4dfb0c9d7200c4b
Armin Sander <armin@replicator.org>
2019-08-28 23:00:04 +0200

    fix some compilation errors of testcases

--- layout/src/layout.rs
+++ layout/src/layout.rs
@@ -234,7 +234,7 @@ mod tests {
         let mut canvas = Drawing::new();
         let paint = paint().color(0xff0000f0);
         let rect = rect((0, 0), (200, 100));
-        canvas.draw(RoundedRect::from((rect, Radius::new(10.0))), &paint);
+        canvas.draw(RoundedRect::from((rect, Radius::new(10.0))), paint);
         canvas.render();
     }
 }
