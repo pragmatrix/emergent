8a7baad86ac742871c802cbb559bec5a140b1bb0
Armin Sander <armin@replicator.org>
2019-12-23 22:38:18 +0100

    some ideas how to render lists

--- presenter/src/component/list.rs
+++ presenter/src/component/list.rs
@@ -1,4 +1,7 @@
-/*
+use crate::Presenter;
+use emergent_drawing::{Bounds, Vector};
+use emergent_presentation::Presentation;
+
 /// A list component. Structures nested presentations in a scrolling list
 /// and renders only those which are visible.
 
@@ -6,30 +9,54 @@ enum ListMsg {
     Pan(Vector),
 }
 
-struct List<Msg, Keys> {
-    /// The top offset of the first element.
-    /// Effectively the transformation of the nested elements.
-    top_offset: usize,
-
+struct List {
     /// Index to the top element.
     top_element: usize,
 
-    HashSet<Keys> keys,
+    /// The top offset of the first element.
+    /// Effectively the transformation of the nested elements.
+    top_offset: usize,
 }
 
-impl List<Msg> {
-    pub fn new() -> List<Msg> {
+#[derive(Clone)]
+struct Cached<Key> {
+    key: Key,
+    presentation: Presentation,
+}
+
+impl List {
+    #[allow(dead_code)]
+    pub fn new() -> List {
         Self {
             top_offset: 0,
-            top_element: 0
+            top_element: 0,
         }
     }
 
-    pub fn present(&mut self, p: &mut Presenter<Msg>, f: impl Fn(&mut Presenter, usize)) {
+    #[allow(dead_code)]
+    pub fn present<Msg, Key>(
+        &mut self,
+        p: &mut Presenter<Msg>,
+        keys: &[Key],
+        f: impl Fn(&mut Presenter<Msg>, &Key, usize),
+    ) where
+        Key: Clone + 'static,
+    {
+        let cache: &mut Vec<Option<Cached<Key>>> = p.resolve(|| Vec::new());
+        // trim cache.
+        if cache.len() > keys.len() {
+            cache.resize(keys.len(), None);
+        }
 
+        let mut top = self.top_offset;
+        let element_index = self.top_element;
+        let height = 1000; //TODO
+        while top < height && element_index < keys.len() {
+            if cache.len() < element_index {
+                cache.resize(element_index + 1, None);
+            }
 
-
-
+            // f(p, &keys[element_index], element_index)
+        }
     }
 }
-*/
